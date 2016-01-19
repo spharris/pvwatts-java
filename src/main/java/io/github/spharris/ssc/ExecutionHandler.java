8724d72a3dabf798de3a4ebfd31ed238b4c30371
Scott Harris <spharris@gmail.com>
2016-01-18 20:36:41 -0800

    Add execution handler

---
+++ src/main/java/io/github/spharris/ssc/ExecutionHandler.java
@@ -0,0 +1,33 @@
+package io.github.spharris.ssc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface ExecutionHandler {
+	public static enum MessageType {
+		NOTICE(1),
+		WARNING(2),
+		ERROR(3);
+		
+		private int sscConst;
+		
+		private static Map<Integer, MessageType> sscVarTypeMap = new HashMap<>();
+		static {
+			for (MessageType v : values()) {
+				sscVarTypeMap.put(v.sscConst, v);
+			}
+		}
+		
+		private MessageType(final int sscConst) {
+			this.sscConst = sscConst; 
+		}
+		
+		public static MessageType forInt(int val) {
+			return sscVarTypeMap.get(val);
+		}
+	}
+	
+	public boolean handleLogMessage(MessageType type, float time, String message);
+	
+	public boolean handleProgressUpdate(float percentComplete, float time, String text);
+}
