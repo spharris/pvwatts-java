8da530ff2814bf14a06687614248802307c7958a
Scott Harris <spharris@gmail.com>
2016-01-18 20:22:14 -0800

    Actually add the file

---
+++ src/main/java/io/github/spharris/ssc/web/UncaughtExceptionHandler.java
@@ -0,0 +1,21 @@
+package io.github.spharris.ssc.web;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class UncaughtExceptionHandler implements ExceptionMapper<Exception> {
+
+	@Override
+	public Response toResponse(Exception exception) {
+		Status s = Status.INTERNAL_SERVER_ERROR;
+		Error e = Error.builder().statusCode(s.getStatusCode()).errorCode("ServerError")
+				.details("An unknown exception occurred")
+				.build();
+		
+		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e).build();
+	}
+	
+}
