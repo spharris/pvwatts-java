f79dd3c96f29c4780f4cd6d8384e2c03ac5fed30
Scott Harris <spharris@gmail.com>
2016-01-20 17:24:45 -0800

    Make SscExecutionHandler package private

--- src/main/java/io/github/spharris/ssc/SscExecutionHandler.java
+++ src/main/java/io/github/spharris/ssc/SscExecutionHandler.java
@@ -8,7 +8,7 @@ import com.sun.jna.Pointer;
  * 
  * @author spharris
  */
-public interface SscExecutionHandler extends Callback {
+interface SscExecutionHandler extends Callback {
 	public boolean update(Pointer module, Pointer sscFunction, int action,
 			float f0, float f1, String s0, String s1, Pointer userData);
 }
