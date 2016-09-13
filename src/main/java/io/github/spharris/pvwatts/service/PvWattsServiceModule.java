e7ff5766bdfec2c992d96d4768842a0fd7e44b77
Scott Harris <spharris@gmail.com>
2016-09-12 17:16:55 -0700

    Install weather module

--- src/main/java/io/github/spharris/pvwatts/service/PvWattsServiceModule.java
+++ src/main/java/io/github/spharris/pvwatts/service/PvWattsServiceModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 
 import io.github.spharris.pvwatts.service.v4.PvWatts4Service;
 import io.github.spharris.pvwatts.service.v5.PvWatts5Service;
+import io.github.spharris.pvwatts.service.weather.WeatherModule;
 import io.github.spharris.ssc.ModuleFactory;
 
 public final class PvWattsServiceModule extends AbstractModule {
@@ -11,6 +12,8 @@ public final class PvWattsServiceModule extends AbstractModule {
   @Override
   protected void configure() {
     requireBinding(ModuleFactory.class);
+    
+    install(new WeatherModule());
     
     bind(PvWatts4Service.class);
     bind(PvWatts5Service.class);
